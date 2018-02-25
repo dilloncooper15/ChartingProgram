cb59ac54395a5755f8e893455ae88246a2ad67cb
dillonc <dillon.cooper@twomen.com>
2018-02-25 16:04:49 -0500

    Refactored Project to include classes

---
+++ src/DropDownList.kt
@@ -0,0 +1,93 @@
+import java.awt.FlowLayout
+import java.awt.RenderingHints
+import java.util.stream.IntStream.range
+import javax.swing.JComboBox
+import javax.swing.JPanel
+import kotlin.coroutines.experimental.ContinuationInterceptor
+
+open class DropDownList {
+    //COMBO BOX
+    //Create a drop down combo box for the groups.
+//    val groupListComboBox: JComboBox<String> = JComboBox()
+
+
+    val dropDownMap = mapOf(
+            "Anger Management" to "Participants discussed the emotion of anger and how it affects the body and mind." +
+                    "We discussed emotional and physical responses of anger, and referenced primary and secondary " +
+                    "emotions concerning anger. Participants identified their current responses to anger, there support" +
+                    "system for when they become angry, events/situations which make them angered, and new ways of" +
+                    "dealing with and expressing their anger. Participants completed the \"Anger Hand\" activity. They" +
+                    "were encouraged to discuss what stops them from trying new ways of anger management techniques," +
+                    "and encouraged to become aware of their reactions both emotionally and physically.\n" +
+                    "Participants discussed the emotion of anger and how it affects the body and mind. We discussed" +
+                    "emotional and physical responses of anger, and referenced primary and secondary emotions concerning" +
+                    "anger. Participants identified their current responses to anger by drawing/coloring where they" +
+                    "experience anger physically within the body and what anger looks like. We discussed the importance" +
+                    "of becoming aware of physical cues to better identify sources of anger and intense emotions.\n",
+            "Art Therapy/Feelings" to "Group members participated in a creative outlet activity based off of the topic" +
+                    "of emotion awareness. Members explored their basic knowledge of emotions and feelings. Members then" +
+                    "were instructed to draw out how they were feeling at that point in time. They were encouraged to" +
+                    "use their creativity and to utilize various lines, shapes, colors, and materials. Soothing music" +
+                    "was played in the background to encourage relaxation. This group activity was done as a combination" +
+                    "activity of the multidisciplinary team members, as group members were encouraged to process their" +
+                    "drawings in talk therapy.\n",
+            "Awareness" to "Group members participated in a baking activity which encouraged functional/practical" +
+                    "skills, as well as self-empowerment and coping skills. While waiting for treats to bake, members" +
+                    "completed a worksheet showcasing several personal domains within the acronym, SPICE. The acronym" +
+                    "SPICE stood for: Social, physical, intellectual, creativity, emotional, and spiritual and/or" +
+                    "surrounding. Participants explored what aspects of each domain they already have present in their" +
+                    "lives, and then what they would like to incorporate more of into their lives. Participants" +
+                    "discussed how they can achieve their goals.\n" +
+                    "Group members were instructed through four various seated yoga stretches to increase focus, relieve" +
+                    "stress, and encourage mental, emotional, and physical well-being.\n" +
+                    "Group members participated in a self-exploration activity to increase self-awareness and esteem." +
+                    "Members explored their personal exploration houses. They were encouraged to identify several" +
+                    "aspects of awareness of self such as personal values and beliefs, areas they would like to" +
+                    "address/change in their lives, emotions they experience, and their support systems. Members were" +
+                    "encouraged to work on their houses one level at a time by building awareness of self in increasing" +
+                    "self-worth.\n",
+            "Co-Occuring" to "",
+            "Communication Skills" to "",
+            "Coping Skills" to "",
+            "Discharge Game" to "",
+            "Emotions" to "",
+            "Empowerment" to "",
+            "Fitness Facts" to "",
+            "Goals" to "",
+            "Impulse Control" to "",
+            "Jenga" to "",
+            "Leisure Education" to "",
+            "Life Skills Game" to "",
+            "Masking" to "",
+            "Mindfulness" to "",
+            "Needs vs Wants" to "",
+            "Nutrition" to "",
+            "Problem Solving" to "",
+            "Relaxation/Stress Management" to "",
+            "Self-Esteem" to "",
+            "Time Management" to "",
+            "Values" to "",
+            "Wellness" to "")
+//
+//    fun dropDownListItemsComboBox() {
+//        val groupListComboBox: JComboBox<String> = JComboBox()
+//        for (i in dropDownMap.keys) { //range(0, dropDownMap.size)) {
+//            println(groupListComboBox.addItem(i))
+//        }
+//        println(dropDownListItemsComboBox())
+//    }
+
+
+
+//    fun createDropDownList() {
+//
+//        //Create the northern panel and specify it be a flow layout.
+//        val northPanel = JPanel(FlowLayout(FlowLayout.LEFT))
+//
+//        //Add the panel to the frame and specify it cover the north area of the frame layout.
+//        MainActivity.frame.add(northPanel, "North")
+//
+//        //Add the groupListHashMap to the north panel.
+//        northPanel.add(groupListComboBox)
+
+}
